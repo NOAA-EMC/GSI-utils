39caeeb73ecdb1a93aeea5d5a2132c727179340d
DavidHuber <David.Huber@noaa.gov>
2023-09-13 11:49:02 +0000

    Upgrade compiler/libraries to Intel 2022/spack-stack #18

--- modulefiles/gsiutils_hera.intel.lua
+++ modulefiles/gsiutils_hera.intel.lua
@@ -1,27 +1,23 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.4.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/jcsda/jedipara/spack-stack/modulefiles")
 
-local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
-local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
-local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.0.4"
-local cmake_ver=os.getenv("cmake_ver") or "3.20.1"
-local anaconda_ver=os.getenv("anaconda_ver") or "2.3.0"
+local stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
+local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
-load(pathJoin("hpc", hpc_ver))
-load(pathJoin("hpc-intel", hpc_intel_ver))
-load(pathJoin("hpc-impi", hpc_impi_ver))
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
-
-prepend_path("MODULEPATH", "/contrib/anaconda/modulefiles")
-
-load(pathJoin("anaconda", anaconda_ver))
 
 load("gsiutils_common")
 
-load(pathJoin("prod_util", prod_util_ver))
+load(pathJoin("prod-util", prod_util_ver))
 
 pushenv("CFLAGS", "-xHOST")
 pushenv("FFLAGS", "-xHOST")
