c6b6429eea40732c1589d9ba348abd9b24cb8aa8
DavidHuber <david.huber@noaa.gov>
2024-02-14 19:02:27 +0000

    Correct Hera-GNU module file.

--- modulefiles/gsiutils_hera.gnu.lua
+++ modulefiles/gsiutils_hera.gnu.lua
@@ -9,7 +9,7 @@ local python_ver=os.getenv("python_ver") or "3.11.6"
 local stack_intel_ver=os.getenv("stack_gcc_ver") or "9.2.0"
 local stack_impi_ver=os.getenv("stack_openmpi_ver") or "4.1.5"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-local openblas_ver=os.getenv("cmake_ver") or "0.3.19"
+local openblas_ver=os.getenv("cmake_ver") or "0.3.24"
 
 load(pathJoin("stack-gcc", stack_gcc_ver))
 load(pathJoin("stack-openmpi", stack_openmpi_ver))
