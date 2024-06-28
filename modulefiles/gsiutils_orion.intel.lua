46b170ac3ddfafc5f4c64419e097281d4d7403e2
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-06-28 10:30:53 -0400

    update orion modulefile for Rocky 8

--- modulefiles/gsiutils_orion.intel.lua
+++ modulefiles/gsiutils_orion.intel.lua
@@ -1,11 +1,16 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+-- Spack Stack installation specs
+local ss_dir="/work/noaa/epic/role-epic/spack-stack/orion"
+local ss_ver=os.getenv("stack_ver") or "1.6.0"
+local ss_env=os.getenv("stack_env") or "gsi-addon-env-rocky9"
+
+prepend_path("MODULEPATH", pathJoin(ss_dir, "spack-stack-" .. ss_ver, "envs", ss_env, "install/modulefiles/Core"))
 
 local stack_python_ver=os.getenv("python_ver") or "3.11.6"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2022.0.2"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
