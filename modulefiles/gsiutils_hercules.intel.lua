d366c801fa61d6077a98e6c519cfa83b363a80f1
David Huber <david.huber@noaa.gov>
2025-06-18 14:25:35 -0500

    Point MSU modules to 191 installations

--- modulefiles/gsiutils_hercules.intel.lua
+++ modulefiles/gsiutils_hercules.intel.lua
@@ -1,16 +1,16 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
-local stack_python_ver=os.getenv("python_ver") or "3.11.6"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
-local mkl_ver=os.getenv("mkl_ver") or "2022.2.1"
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+local stack_python_ver=os.getenv("python_ver") or "3.11.7"
+local stack_intel_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local mkl_ver=os.getenv("mkl_ver") or "2024.2.1"
+local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
-load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-oneapi", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 load(pathJoin("intel-oneapi-mkl", mkl_ver))
 load(pathJoin("python", stack_python_ver))
