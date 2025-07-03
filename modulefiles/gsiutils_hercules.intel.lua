7cd4a3ec644ceb69b420154a4c42f10d5725e0fa
David Huber <david.huber@noaa.gov>
2025-07-03 13:37:13 -0400

    Correct module paths for Hera and MSU

--- modulefiles/gsiutils_hercules.intel.lua
+++ modulefiles/gsiutils_hercules.intel.lua
@@ -4,14 +4,14 @@ help([[
 prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
 local stack_python_ver=os.getenv("python_ver") or "3.11.7"
-local stack_intel_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
 local mkl_ver=os.getenv("mkl_ver") or "2024.2.1"
 local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
-load(pathJoin("stack-oneapi", stack_intel_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 load(pathJoin("intel-oneapi-mkl", mkl_ver))
 load(pathJoin("python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
