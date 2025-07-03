6803e4801cd4c04e8dbb3878a5fba2685555e0a1
David Huber <david.huber@noaa.gov>
2025-07-03 13:59:41 -0400

    Update ip to 5.2.0 on wcoss2

--- modulefiles/gsiutils_wcoss2.intel.lua
+++ modulefiles/gsiutils_wcoss2.intel.lua
@@ -12,11 +12,18 @@ local prod_util_ver=os.getenv("prod_util_ver") or "2.0.10"
 local hdf5_ver=os.getenv("hdf5_ver") or "4.9.2"
 local pnetcdf_ver=os.getenv("pnetcdf_ver") or "1.12.2"
 local netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
-local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
+local bufr_ver=os.getenv("bufr_ver") or "12.1.0"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
-local sp_ver=os.getenv("sp_ver") or "2.3.3"
-local ip_ver=os.getenv("ip_ver") or "3.3.3"
+--load(pathJoin("ip", ip_ver))
+-- Temporarily define IP's paths here.
+-- TODO when testing is complete, request an official installation in https://github.com/NOAA-EMC/WCOSS2-requests/issues/11
+pushenv("ip_ROOT", pathJoin("/apps/ops/para/libs/intel/19.1.3.304/ip", ip_ver))
+pushenv("IP_INC4", pathJoin("/apps/ops/para/libs/intel/19.1.3.304/ip", ip_ver, "include_4"))
+pushenv("IP_INCd", pathJoin("/apps/ops/para/libs/intel/19.1.3.304/ip", ip_ver, "include_d"))
+pushenv("IP_LIB4", pathJoin("/apps/ops/para/libs/intel/19.1.3.304/ip", ip_ver, "lib64/libip_4.a"))
+pushenv("IP_LIBd", pathJoin("/apps/ops/para/libs/intel/19.1.3.304/ip", ip_ver, "lib64/libip_d.a"))
+pushenv("ip_VERSION", ip_ver)
 local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
 local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
 local nemsio_ver=os.getenv("nemsio_ver") or "2.5.4"
@@ -40,7 +47,6 @@ load(pathJoin("netcdf-D", netcdf_ver))
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
-load(pathJoin("sp", sp_ver))
 load(pathJoin("ip", ip_ver))
 load(pathJoin("sigio", sigio_ver))
 load(pathJoin("sfcio", sfcio_ver))
