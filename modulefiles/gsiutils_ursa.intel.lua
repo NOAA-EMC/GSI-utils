be49727ba817632a9b73cc8090e1602995dfe2cd
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2026-01-30 20:23:03 +0000

    initial update to spack-stack/2.0.0, replace bacio_4 with bacio (#90)

--- modulefiles/gsiutils_ursa.intel.lua
+++ modulefiles/gsiutils_ursa.intel.lua
@@ -1,28 +1,19 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-2.0.0/envs/ue-oneapi-2025.2.1/modules/Core")
 
-local python_ver=os.getenv("python_ver") or "3.11.7"
-local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
-local stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or "2021.13"
-local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
-local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2025.2.1"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+local oneapi_mkl_ver=os.getenv("oneapi_mkl_ver") or "2025.2.0"
+local cmake_ver=os.getenv("cmake_ver") or "3.31.8"
+local crtm_fix_ver=os.getenv("crtm_fix_ver") or "2.4.0.2"
 
-load(pathJoin("stack-oneapi", stack_oneapi_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
-load(pathJoin("python", python_ver))
+load(pathJoin("stack-intel-oneapi-compilers", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("intel-oneapi-mkl", oneapi_mkl_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsiutils_common")
 
-load(pathJoin("prod_util", prod_util_ver))
-
-pushenv("CFLAGS", "-xHOST")
-pushenv("FFLAGS", "-xHOST")
-
-setenv("CC","mpiicc")
-setenv("CXX","mpiicpc")
-setenv("FC","mpiifort")
-
-whatis("Description: GSI utilities environment on Hera with Intel Compilers")
+whatis("Description: GSI Utilities environment on Ursa with Intel Compilers")
